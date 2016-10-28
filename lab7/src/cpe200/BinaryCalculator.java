42459de52b5142dbc0e93da93550e2348cd0b365
MisterMu <painter.suwannawat@gmail.com>
2016-10-28 16:54:11 +0700

    Complete Lab7_2

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,37 +1,79 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
+    private BigDecimal tmp_BigDecimal;
 
     public BinaryCalculator() {
     }
 
-    public void setFirstOperand(IOperand operand) {
+    public void setFirstOperand(IOperand firstOperand) {
+        this.firstOperand = new BigDecimal(binToDec(firstOperand));
     }
 
-    public void setSecondOperand(IOperand operand) {
+    public void setSecondOperand(IOperand secondOperand) {
+        this.secondOperand = new BigDecimal(binToDec(secondOperand));
+    }
+
+    public BigDecimal getFirstOperand() {
+        return firstOperand;
+    }
+
+    public BigDecimal getSecondOperand() {
+        return secondOperand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException();
+        }
+        tmp_BigDecimal = new BigDecimal(getFirstOperand().add(getSecondOperand()).stripTrailingZeros().toString());
+        return decToBin(tmp_BigDecimal);
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException();
+        }
+        tmp_BigDecimal = new BigDecimal(getFirstOperand().subtract(getSecondOperand()).stripTrailingZeros().toString());
+        return decToBin(tmp_BigDecimal);
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException();
+        }
+        tmp_BigDecimal = new BigDecimal(getFirstOperand().multiply(getSecondOperand()).stripTrailingZeros().toString());
+        return decToBin(tmp_BigDecimal);
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException();
+        } else if (getSecondOperand().equals("0")) {
+            throw new ArithmeticException();
+        }
+        tmp_BigDecimal = new BigDecimal(getFirstOperand().divide(getSecondOperand(),5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString());
+        return decToBin(tmp_BigDecimal);
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException();
+        }
+        tmp_BigDecimal = new BigDecimal(getFirstOperand().pow(getSecondOperand().intValue()).stripTrailingZeros().toString());
+        return decToBin(tmp_BigDecimal);
     }
 
+    private Integer binToDec(IOperand bin) {
+        return Integer.parseInt(bin.getOperand(),2);
+    }
+
+    private String decToBin(BigDecimal dec) {
+        return Integer.toBinaryString(dec.intValue());
+    }
 }

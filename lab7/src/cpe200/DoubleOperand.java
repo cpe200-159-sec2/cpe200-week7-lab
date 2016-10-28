95b51f38a58549ed244e35ef00a13a50784e069e
cwm <warakornchamp@gmail.com>
2016-10-28 15:42:28 +0700

    Finish lab 1

--- lab7/src/cpe200/DoubleOperand.java
+++ lab7/src/cpe200/DoubleOperand.java
@@ -3,14 +3,21 @@ package cpe200;
 /**
  * Created by pruet on 26/9/2559.
  */
-public class DoubleOperand implements IOperand{
+public class DoubleOperand implements IOperand {
+
+    private String operand;
 
     public DoubleOperand(Double d) {
+        this.setOperand(Double.toString(d));
+    }
 
+    public void setOperand(String operand) {
+        this.operand = operand;
     }
 
     @Override
     public String getOperand() {
-        return null;
+        return operand;
     }
+
 }

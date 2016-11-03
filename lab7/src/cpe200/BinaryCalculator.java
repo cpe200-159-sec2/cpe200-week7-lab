1885f88e9f13090d40347770168c42d8839242a2
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-11-03 19:08:31 +0700

    fix form lab 1 + Add BaseCalculator

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,5 +1,8 @@
 package cpe200;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
     public IOperand firstOperand;
     public IOperand secondOperand;
@@ -8,30 +11,67 @@ public class BinaryCalculator {
     }
 
     public void setFirstOperand(IOperand operand) {
+        firstOperand = operand;
     }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.add(two).stripTrailingZeros().toString();
+        }
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.subtract(two).stripTrailingZeros().toString();
+        }
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.multiply(two).stripTrailingZeros().toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.divide(two,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
+        }
     }
 
     public String power() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum() == -1 || two.signum() == -1){
+            throw new RuntimeException();
+        }else{
+            return one.pow(two.intValueExact()).stripTrailingZeros().toString();
+        }
     }
 
 }

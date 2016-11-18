e3da5bb9bfcf08d7c1275bb6f31dadb0d718d438
GEST <naluedomgest@gmail.com>
2016-11-18 13:01:39 +0700

    LAB 7.1

--- lab7/src/cpe200/IntegerOperand.java
+++ lab7/src/cpe200/IntegerOperand.java
@@ -4,13 +4,13 @@ package cpe200;
  * Created by pruet on 26/9/2559.
  */
 public class IntegerOperand implements IOperand {
-
+    private int operand;
     public IntegerOperand(int i) {
-
+        operand = i;
     }
 
     @Override
     public String getOperand() {
-        return null;
+        return Integer.toString(operand);
     }
 }

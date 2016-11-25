348f2ceea476c1df44bd9138b377ace8f80f45ac
Pair <roybura.b@gmail.com>
2016-11-26 00:47:23 +0700

    lab3

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -3,79 +3,77 @@ package cpe200;
 /**
  * Created by Pair on 25/11/2559.
  */
-import java.math.BigDecimal;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
-public class BinaryCalculator {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BinaryCalculator extends BaseCalculator{
     private BigDecimal firstOperand;
     private BigDecimal secondOperand;
 
     public BinaryCalculator(){
-
+        setFirstOperand(new BigDecimal(0));
+        setSecondOperand(new BigDecimal(0));
     }
 
     public boolean isBinary(IOperand operand){
         return operand.getOperand().matches("^[01]+");
     }
 
-    public void setFirstOperand(IOperand operand){
-        if(isBinary(operand)){
-            firstOperand = new BigDecimal(operand.getOperand());
-        }else{
-            throw new ArithmeticException("This isn't Binary!!");
-        }
+    public void setFirstOperand(IOperand operand) {
+        Binarycheck(operand);
+        setFirstOperand(new BigDecimal(Integer.parseInt(operand.getOperand(), 2)));
     }
 
-    public void setSecondOperand(IOperand operand){
-        if(isBinary(operand)){
-            secondOperand = new BigDecimal(operand.getOperand());
-        }else{
-            throw new ArithmeticException("This isn't Binary!!");
-        }
+    public void setSecondOperand(IOperand operand) {
+        Binarycheck(operand);
+        setFirstOperand(new BigDecimal(Integer.parseInt(operand.getOperand(), 2)));
     }
 
     public String add() throws RuntimeException{
-        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
-            throw new RuntimeException("Must be Non Negative Number");
-        }else{
-            return firstOperand.add(secondOperand).stripTrailingZeros().toString();
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
         }
+        return Integer.toBinaryString(getFirstOperand().add(getFirstOperand()).stripTrailingZeros().intValue());
     }
 
     public String subtract() throws RuntimeException {
-        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
-            throw new RuntimeException("Must be Non Negative Number");
-        }else{
-            return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
         }
+        return Integer.toBinaryString(getFirstOperand().subtract(getFirstOperand()).stripTrailingZeros().intValue());
     }
 
     public String multiply() throws RuntimeException {
-        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
-            throw new RuntimeException("Must Be Non-Negative Number");
-        } else {
-            return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
-            }
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
         }
+        return Integer.toBinaryString(getFirstOperand().multiply(getFirstOperand()).stripTrailingZeros().intValue());
+    }
 
     public String division() throws RuntimeException {
-        String Ans;
-        if(firstOperand.intValue()<0||secondOperand.intValue()<0){
-            throw new RuntimeException("Must Be Non-Negative Number");
-        }else {
-            if (secondOperand.intValue() == 0) {
-                throw new ArithmeticException("Divided by Zero");
-            } else {
-                Ans = firstOperand.divide(secondOperand, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
-            }
-            return Ans;
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException("negative value");
         }
+        if (getSecondOperand().intValue() == 0) {
+            throw new RuntimeException("Divide by Zero");
+        }
+        return Integer.toBinaryString(getFirstOperand().divide(getSecondOperand(), 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros().intValue());
     }
-public String power() throws RuntimeException {
-    if(firstOperand.intValue()<0||secondOperand.intValue()<0){
-        throw  new RuntimeException("Must Be Non-Negative Number");
-    }else {
-        return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
+
+    public String power() throws RuntimeException {
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
+        }
+            return Integer.toBinaryString(getFirstOperand().pow(getSecondOperand().intValue()).stripTrailingZeros().intValue());
     }
-}
+
+    private void Binarycheck(IOperand check) throws ArithmeticException{
+        if(check.getOperand().matches("[01]+"));
+        else throw new ArithmeticException("Not Binary!");
+    }
 
 }
+
+
--- lab7/src/cpe200/DecimalCalculator.java
+++ lab7/src/cpe200/DecimalCalculator.java
@@ -1,37 +1,57 @@
 package cpe200;
 
-public class DecimalCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+import java.math.BigDecimal;
+
+public class DecimalCalculator extends BaseCalculator {
+    private IOperand firstOperand;
+    private IOperand secondOperand;
 
     public DecimalCalculator() {
+        setFirstOperand(new BigDecimal(0));
+        setSecondOperand(new BigDecimal(0));
     }
 
     public void setFirstOperand(IOperand operand) {
+
     }
 
     public void setSecondOperand(IOperand operand) {
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
+        }
+        return getFirstOperand().add(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue() <0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
+        }
+        return getFirstOperand().subtract(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue() <0 || getSecondOperand().intValue() <0){
+            throw new RuntimeException("Negative value");
+        }
+        return getFirstOperand().multiply(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue() <0){
+            throw new RuntimeException("Negative value");
+        }
+        return getFirstOperand().divide(getSecondOperand(),5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("Negative value");
+        }
+        return getFirstOperand().pow(getSecondOperand().intValue()).stripTrailingZeros().toString();
     }
 
 }
